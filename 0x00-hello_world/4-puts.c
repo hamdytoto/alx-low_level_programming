aa8fc5203d680259a68f5b479d02737324270ae2
hamdytoto <hamdytoto552@gmail.com>
2023-03-25 08:25:13 -0700

    the 4 comit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Description:A c program that prints with puts function 
+ * Return always 0(Success)
+h*/
+int main (void ) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
